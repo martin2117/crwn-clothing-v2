81fdb706d96757685d59a9bd589e2b3b841623c8
homolama <martin.homola2@merck.com>
2023-02-02 17:31:08 +0100

    Add navigation component and styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from "./components/routes/home/home.component";
+import Navigation from './components/routes/navigation/navigation.component';
+
+
+const Shop = () => {
+  return (
+    <h1>I am the shop.</h1 >
+  )
+};
 
 const App = () => {
 
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+      </Route>
     </Routes>
   )
 };
